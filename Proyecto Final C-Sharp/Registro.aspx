8e5c1290888db4508b818e61fba3df13cbff96ea
amourou <amourou@ies-eugeni.cat>
2022-04-12 10:11:41 +0200

    Plantilla conteneder de arriba

---
+++ Proyecto Final C-Sharp/Registro.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registro.aspx.cs" Inherits="Proyecto_Final_C_Sharp.Registro" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link rel="stylesheet" href="CSSapp/RegistroLogin.css"/>
+</head>
+<body>
+</body>
+</html>
--- Proyecto Final C-Sharp/Registro.aspx.designer.cs
+++ Proyecto Final C-Sharp/Registro.aspx.designer.cs
@@ -11,16 +11,7 @@ namespace Proyecto_Final_C_Sharp
 {
 
 
-    public partial class SiteMaster
+    public partial class Registro
     {
-
-        /// <summary>
-        /// Control MainContent.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
