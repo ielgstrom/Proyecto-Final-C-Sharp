1ac59e8dcb6e3b1434f91d33b9646a0dc36d7c89
amourou <amourou@ies-eugeni.cat>
2022-04-14 13:27:42 +0200

    commit registro acabado

--- Proyecto Final C-Sharp/RegisterPage.aspx.cs
+++ Proyecto Final C-Sharp/RegisterPage.aspx.cs
@@ -14,8 +14,11 @@ namespace Proyecto_Final_C_Sharp
     public partial class Registro : System.Web.UI.Page
     {
         public User usuario = null;
+        SqlConnection connection = null;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            connection = DAL.DBConnection.ConnectLearnifyDB();
             mensajeError.Text = " ";
         }
 
@@ -26,54 +29,56 @@ namespace Proyecto_Final_C_Sharp
 
         private void comprobarDatos()
         {
-            SqlConnection connection = DAL.DBConnection.ConnectLearnifyDB();
-            bool validacion = false;
-
+            // Cogemos los valores de los inputs
             string nombre = Request["inputNombre"];
             string nombreUsuario = Request["inputNombreUsuario"];
             string email = Request["inputEmail"];
             string password = Request["inputContra"];
             string repitePassword = Request["inputRepetirContra"];
 
-            if (String.IsNullOrEmpty(nombre) || String.IsNullOrEmpty(nombreUsuario) ||
-                String.IsNullOrEmpty(email)|| String.IsNullOrEmpty(password)|| String.IsNullOrEmpty(repitePassword))
-            {
+            // Revisamos que no hayan campor vacios
+            if (String.IsNullOrEmpty(nombre) || String.IsNullOrEmpty(nombreUsuario) || String.IsNullOrEmpty(email)|| String.IsNullOrEmpty(password)|| String.IsNullOrEmpty(repitePassword))
                 mensajeError.Text = "Rellena todos los campos";
-            }
             else
             {
-                usuario = new User();
-
-                foreach(User user in usuario.Read(connection))
-                {
-                    if (nombreUsuario == user.Username)
-                    {
-                        validacion = true;
-                    }
-                }
-
-                if(validacion)
-                {
-                    mensajeError.Text = "El nombre de usuario ya existe";
-                }
+                // Si el nombre existe no se añade
+                if(existeNombreUsuario(nombreUsuario))
+                    mensajeError.Text = "El nombre de usuario ya existe, escoge otro";
                 else
                 {
-                    if (!comprobarContraseña(password))
-                        mensajeError.Text = "La contraseña no contiene los valores pedidos";
+                    // Si la cuenta existe, que inicie sesion
+                    if (cuentaExistente(email))
+                        mensajeError.Text = "Ya estas registrado, Inicia Sesión";
                     else
                     {
-                        if (password == repitePassword)
-                        {
-                            usuario = new User(email, nombreUsuario, password, nombre, null);
-                            usuario.Insert(connection);
-                        }
+                        // Comprobamos todos los valores de la contraseña
+                        if (!comprobarContraseña(password))
+                            mensajeError.Text = "La contraseña no contiene los valores pedidos";
                         else
-                            mensajeError.Text = "Las contraseñas no coinciden";
+                        {
+                            if (password != repitePassword)
+                            {
+                                mensajeError.Text = "Las contraseñas no coinciden";
+                            }
+                            else
+                            {
+                                // Si acepta las condiciones se crea la cuenta
+                                if (inputPoliticaPrivacidad.Checked)
+                                {
+                                    usuario = new User(email, nombreUsuario, Seguridad.Encriptar(password), nombre, null);
+                                    usuario.Insert(connection);
+                                }
+                                else
+                                    mensajeError.Text = "Acepta las condiciones y la politica de privacidad";
+                            }
+                        }
                     }
                 }
             }
         }
 
+        #region METODOS
+        // Metodo que comprueba que la contraseña contenga numero, mayuscula, minuscula y caracter especial
         public bool comprobarContraseña(string contra)
         {
             var formato = @"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).*$";
@@ -82,5 +87,41 @@ namespace Proyecto_Final_C_Sharp
 
             return reg.IsMatch(contra);
         }
+         
+        // Metodo que comprueba si existe el nombre de usuario
+        public bool existeNombreUsuario(string nombreUsuario)
+        {
+            bool validacion = false;
+            usuario = new User();
+
+            foreach (User user in usuario.Read(connection))
+            {
+                if (nombreUsuario == user.Username)
+                {
+                    validacion = true;
+                }
+            }
+
+            return validacion;
+        }
+
+        // Metodo que comprueba si ya existe la cuenta
+        private bool cuentaExistente(string email)
+        {
+            bool validacion = false;
+            usuario = new User();
+
+            foreach (User user in usuario.Read(connection))
+            {
+                if (email == user.Email)
+                {
+                    validacion = true;
+                }
+            }
+
+            return validacion;
+        }
+        #endregion
+
     }
 }
