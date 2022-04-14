5cecf856f7e1d5227150c1ed8889d2e8c5660411
amourou <amourou@ies-eugeni.cat>
2022-04-14 10:35:10 +0200

    commit btsignup no acabado

--- Proyecto Final C-Sharp/RegisterPage.aspx.cs
+++ Proyecto Final C-Sharp/RegisterPage.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Proyecto_Final_C_Sharp.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,36 @@ namespace Proyecto_Final_C_Sharp
 {
     public partial class Registro : System.Web.UI.Page
     {
+        public User usuario = null;
         protected void Page_Load(object sender, EventArgs e)
         {
+            mensajeError.Text = " ";
+        }
 
+        protected void btSignUp_Click(object sender, System.EventArgs e)
+        {
+            comprobarDatos();
+        }
+
+        private void comprobarDatos()
+        {
+            SqlConnection connection = DAL.DBConnection.ConnectLearnifyDB();
+
+            string nombre = Request["inputNombre"];
+            string nombreUsuario = Request["inputNombreUsuario"];
+            string email = Request["inputEmail"];
+            string password = Request["inputContra"];
+            string repitePassword = Request["inputRepetirContra"];
+
+            if(String.IsNullOrEmpty(nombre) || String.IsNullOrEmpty(nombreUsuario) ||
+                String.IsNullOrEmpty(email)|| String.IsNullOrEmpty(password)|| String.IsNullOrEmpty(repitePassword))
+            {
+                mensajeError.Text = "Rellena todos los campos";
+            }
+            else
+            {
+                
+            }
         }
     }
 }
--- Proyecto Final C-Sharp/RegisterPage.aspx.designer.cs
+++ Proyecto Final C-Sharp/RegisterPage.aspx.designer.cs
@@ -13,5 +13,131 @@ namespace Proyecto_Final_C_Sharp
 
     public partial class Registro
     {
+
+        /// <summary>
+        /// Control tituloLogin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl tituloLogin;
+
+        /// <summary>
+        /// Control imgLogin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlImage imgLogin;
+
+        /// <summary>
+        /// Control textoSignUp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl textoSignUp;
+
+        /// <summary>
+        /// Control subtextoSignUp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl subtextoSignUp;
+
+        /// <summary>
+        /// Control inputNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl inputNombre;
+
+        /// <summary>
+        /// Control inputNombreUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl inputNombreUsuario;
+
+        /// <summary>
+        /// Control inputEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl inputEmail;
+
+        /// <summary>
+        /// Control inputContra.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputPassword inputContra;
+
+        /// <summary>
+        /// Control contraHelp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl contraHelp;
+
+        /// <summary>
+        /// Control inputRepetirContra.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputPassword inputRepetirContra;
+
+        /// <summary>
+        /// Control rContraHelp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl rContraHelp;
+
+        /// <summary>
+        /// Control inputPoliticaPrivacidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputCheckBox inputPoliticaPrivacidad;
+
+        /// <summary>
+        /// Control mensajeError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label mensajeError;
+
+        /// <summary>
+        /// Control btSignUp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btSignUp;
     }
 }
