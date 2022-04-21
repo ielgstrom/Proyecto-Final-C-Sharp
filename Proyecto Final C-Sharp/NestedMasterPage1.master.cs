150bc5e9a458a5815f45000aa88641fe1fe2f1ed
Ignasi Elgström Puyuelo <83967813+ielgstrom@users.noreply.github.com>
2022-04-21 11:41:35 +0200

    Explorador de foros acabado

--- Proyecto Final C-Sharp/NestedMasterPage1.master.cs
+++ Proyecto Final C-Sharp/NestedMasterPage1.master.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Proyecto_Final_C_Sharp.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,17 @@ namespace Proyecto_Final_C_Sharp
 {
     public partial class NestedMasterPage1 : System.Web.UI.MasterPage
     {
+        Message mensajes = new Message();
+        List<String> listaTopics = null;
+        SqlConnection connection = null;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            connection = DAL.DBConnection.ConnectLearnifyDB();
+            listaTopics = mensajes.Topics(connection);
+            for (int i = 0; i < listaTopics.Count; i++)
+            {
+                listForos.Controls.Add(new Literal() { Text = $"<a class='forosLinks' href='Foro.aspx?topic={listaTopics[i]}'>{listaTopics[i]}</a>" });
+            }
         }
     }
 }
